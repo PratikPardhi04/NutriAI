c0e49fed458f6bebd65b0771d95601ee85c86057
PratikPardhi04 <pratikpardhi474@gmail.com>
2026-04-27 05:39:27 +0530

    login errror resolved

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import useAuthStore from '../store/authStore';
 
 const api = axios.create({
-  baseURL: `http://${window.location.hostname}:5000/api`, 
+  baseURL: import.meta.env.VITE_API_BASE_URL || (import.meta.env.DEV ? 'http://localhost:5000/api' : '/api'),
 });
 
 api.interceptors.request.use((config) => {
